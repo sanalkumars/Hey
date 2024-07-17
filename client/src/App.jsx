aacf6959b57f6293e8fedf73ed39d802d9e7eab0
Gibi paul <info@nubicus.com>
2024-07-17 18:18:06 +0530

    auth page ui part finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import AuthPage from './pages/Auth'
+import ChatPage from './pages/Chat'
+import ProfilePage from './pages/Profile'
 
 const App = () => {
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth' element={ <AuthPage /> } />
+        <Route path='*' element={<Navigate to={'/auth'} />} />
+        <Route path='/chat' element={ <ChatPage /> } />
+        <Route path='/profile' element={ <ProfilePage /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
